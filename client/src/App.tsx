5084bbba28331b18b2cca09dc7f4f7191627b6af
Melisaysnuh <melissacessna@yahoo.com>
2024-11-07 18:43:10 +0100

    feat: front-end word list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+
 
   return (
     <>
+      <div id='main-container'>
+        <nav id='navigation'><img src='../public/logo.svg' alt='logo' /></nav>
+        <div id='subhead'>
+          <div id='game-container' className='block'>
+            Here is the game container
+          </div>
+          <div id='word-list-container' className='block'>
+            <div id='progress-bar-container'>prog bar
 
+            </div>
+            <div id='word-list'>
+
+              <ul>
+                <li>Allay</li>
+                <li>Allot</li>
+                <li>Allotropy</li>
+                <li>Allyl</li>
+                <li>Altar</li>
+                <li>Aorta</li>
+                <li>Aport</li>
+                <li>Appall</li>
+                <li>Apparat</li>
+                <li>Apply</li>
+                <li>Appro</li>
+                <li>Aptly</li>
+                <li>Arroyo</li>
+                <li>Aryl</li>
+                <li>Atlatl</li>
+                <li>Atoll</li>
+                <li>Atopy</li>
+                <li>Attar</li>
+                <li>Lota</li>
+                <li>Loyal</li>
+                <li>Loyalty</li>
+                <li>Lyart</li>
+                <li>Olla</li>
+                <li>Opal</li>
+                <li>Orally</li>
+                <li>Orator</li>
+                <li>Palatal</li>
+                <li>Pallor</li>
+                <li>Pally</li>
+                <li>Paltry</li>
+                <li>Paly</li>
+                <li>Papa</li>
+                <li>Papal</li>
+                <li>Papaya</li>
+                <li>Pappy</li>
+                <li>Parol</li>
+                <li>Parr</li>
+                <li>Parry</li>
+                <li>Partly</li>
+                <li>Patty</li>
+                <li>Payola</li>
+                <li>Payroll</li>
+                <li>Poplar</li>
+                <li>Poppa</li>
+                <li>Portray</li>
+                <li>Portrayal</li>
+                <li>Potato</li>
+                <li>Prat</li>
+                <li>Pray</li>
+                <li>Proa</li>
+                <li>Prototypal</li>
+                <li>Pyrola</li>
+                <li>Rapport</li>
+                <li>Rapt</li>
+                <li>Raptor</li>
+                <li>Rattly</li>
+                <li>Rattrap</li>
+                <li>Ratty</li>
+                <li>Rota</li>
+                <li>Rotator</li>
+                <li>Rotatory</li>
+                <li>Royally</li>
+                <li>Royalty</li>
+                <li>Tall</li>
+                <li>Taproot</li>
+                <li>Taro</li>
+                <li>Tarot</li>
+                <li>Tarp</li>
+                <li>Tarty</li>
+                <li>Tatty</li>
+                <li>Tola</li>
+                <li>Tolar</li>
+                <li>Torta</li>
+                <li>Totally</li>
+                <li>Tray</li>
+                <li>Typal</li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
