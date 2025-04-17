f16b7ab5f43915b7b7753a6875e8cd5e935a09de
Melisaysnuh <melissacessna@yahoo.com>
2025-04-17 16:28:13 +0200

    initial responsive styling

--- client/src/components/word-list-component.tsx
+++ client/src/components/word-list-component.tsx
@@ -113,8 +113,8 @@ const horizontalPoints = "8,20 58,20 108,20 158,20 208,20 258,20 308,20 358,20 4
                 <div id='word-list-container'>
                     <div className='progress-bar-container'>
                         <img alt='progress spider' id='prog-spider' className={spiderClass} src='./placeholder-spider.svg'></img>
-                        <svg id='prog-line' height="450" width="50">
-                            <polyline className="dotted-line" points="20,8 20,58 20,108 20,158 20,208 20,258 20, 308 20, 358 20, 408" />
+                        <svg id='prog-line' height={isHorizontal? 50 : 450} width={isHorizontal ? 300 : 50}>
+                            <polyline className="dotted-line" points={isHorizontal?horizontalPoints : verticalPoints} />
                             <marker id="circle-marker" markerWidth="6" markerHeight="6" refX="3" refY="3">
                                 <circle className="foreground" cx="3" cy="3" r="2" />
                             </marker>
