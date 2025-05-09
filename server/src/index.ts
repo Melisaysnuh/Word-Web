53c5036014ce02dc1b4531dfdbf34e81e20151ff
Melisaysnuh <melissacessna@yahoo.com>
2025-05-09 15:09:42 +0200

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,7 +40,7 @@ export const app = createApp({
     submitWordController,
     authMiddleware
 });
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 (async () => {
     await connectDB();
     app.listen(port, () => {
