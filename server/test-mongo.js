8a2035d057057d7bdb0fd0118851376e166284e0
Melisaysnuh <melissacessna@yahoo.com>
2025-05-09 14:32:28 +0200

    small test

---
+++ server/test-mongo.js
@@ -0,0 +1,21 @@
+// test-mongo.js
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const url = process.env.DATABASE_URL;
+console.log('url is', url)
+
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('✅ Connected to MongoDB!');
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1);
+  });
